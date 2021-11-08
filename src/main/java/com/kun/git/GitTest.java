8e3282b458f8f8ad4cdd1bf162f8ea0568b4c0d6
jack-ikun <jackkun@aliyun.com>
2021-11-08 14:35:16 +0800

    second commit

--- src/main/java/com/kun/git/GitTest.java
+++ src/main/java/com/kun/git/GitTest.java
@@ -3,11 +3,14 @@ package com.kun.git;
 /**
  * 忽略文件配置成功，灰色
  * 右键项目根目录，Git，add，添加到暂存区，变绿
+ * 右键项目根目录，Git,commit diretory,变黑 提交完成
+ * 代码修改，文件名变蓝 代表：被追踪过，但是被修改 -->需要add commit
  * @author jiakun
  * @create 2021-11-08-14:25
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello IDEA Git");
+        System.out.println("Hello IDEA Git1");
+        System.out.println("Hello IDEA Git2 version reset hard");
     }
 }
