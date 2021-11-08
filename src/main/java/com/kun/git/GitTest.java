7c47dd53ffe8bf5bb95565f1629d79a1052f5805
jack-ikun <jackkun@aliyun.com>
2021-11-08 14:52:41 +0800

    four commit

--- src/main/java/com/kun/git/GitTest.java
+++ src/main/java/com/kun/git/GitTest.java
@@ -5,7 +5,7 @@ package com.kun.git;
  * 右键项目根目录，Git，add，添加到暂存区，变绿
  * 右键项目根目录，Git,commit diretory,变黑 提交完成
  * 代码修改，文件名变蓝 代表：被追踪过，但是被修改 -->需要add commit(idea中可以直接提交到本地库)
- * head （黄色，控制版本，选择分支） master（绿色，版本控制）
+ * head （黄色，控制历史版本，选择分支） master（绿色，当前分支master最新版本）
  * @author jiakun
  * @create 2021-11-08-14:25
  */
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("Hello IDEA Git1");
         System.out.println("Hello IDEA Git2 version reset hard");
         System.out.println("Hello IDEA Git3 version reset hard");
+        System.out.println("Hello IDEA Git4 version reset hard");
         System.out.println("Hello IDEA Git4 version reset hard");
     }
 }
