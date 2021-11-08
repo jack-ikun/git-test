9668960644656e4d6189b3d3dc649bc0704fce38
jack-ikun <jackkun@aliyun.com>
2021-11-08 14:31:12 +0800

    idea project git-test first commit

---
+++ src/main/java/com.kun.bean/User.java
@@ -0,0 +1,11 @@
+package com.kun.bean;
+
+/**
+ * @author jiakun
+ * @create 2021-11-08-14:26
+ */
+public class User {
+    private String username;
+    private String id;
+
+}
---
+++ src/main/java/com/kun/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.kun.git;
+
+/**
+ * 忽略文件配置成功，灰色
+ * 右键项目根目录，Git，add，添加到暂存区，变绿
+ * @author jiakun
+ * @create 2021-11-08-14:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA Git");
+    }
+}
