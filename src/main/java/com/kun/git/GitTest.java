ca93af3afd133f9c201bf963e3a15a583d333653
jack-ikun <jackkun@aliyun.com>
2021-11-08 17:04:43 +0800

    hot-fix commit2

--- src/main/java/com/kun/git/GitTest.java
+++ src/main/java/com/kun/git/GitTest.java
@@ -18,5 +18,6 @@ public class GitTest {
         System.out.println("Hello IDEA Git4 version reset hard");
         System.out.println("Hello IDEA Git4 version reset hard");
         System.out.println("Hello IDEA Git6 version normal merge");
+        System.out.println("Hello IDEA Git6 version error  merge");
     }
 }
